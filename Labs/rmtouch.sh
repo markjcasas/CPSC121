54de4e126e16942db30967afbc30ee3f4e6be5cf
Zachary Peskin <zpeskin@csu.fullerton.edu>
2019-12-07 23:28:11 -0800

    commit

--- Labs/rmtouch.sh
+++
@@ -1,2 +0,0 @@
-rm -rf '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/count-odd.cpp' '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/count-odd.hpp' '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/main.cpp' '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/main' 
-touch '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/count-odd.cpp' '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/count-odd.hpp' '/home/zach/Dropbox (CSU Fullerton)/CPSC121/Labs/Lab_08/prob01/main.cpp' 
