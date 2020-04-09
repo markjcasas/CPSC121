e865ac04aea1a16fed195380f0ac7baa779fb838
Mark Icasas <mark.icasas@csu.fullerton.edu>
2020-04-09 12:56:20 -0700

    Update main.cpp

--- Labs/Lab_05/Answers/prob04/main.cpp
+++ Labs/Lab_05/Answers/prob04/main.cpp
@@ -1,31 +1,64 @@
-#include <fstream>
-#include <iomanip>
+//
+//  main.cpp
+//  Phonebook Display
+//
+//  Created by Mark Icasas on 4/9/20.
+//  Copyright © 2020 Mark Icasas. All rights reserved.
+//
+
 #include <iostream>
+#include <fstream>
 #include <string>
-int main() {
-  std::string file;
-  std::cout << "Please provide the file name for your donation file: ";
-  std::cin >> file;
-  std::ifstream infile;
-  infile.open(file);
-  if (infile.is_open()) {
-    int number;
-    infile >> number;
-    infile.seekg(1, infile.beg);
-    float line;
-    float sum = 0;
-    for (int i = 1; i <= number; i++) {
-      std::cout << "Donation " << i << ": $";
-      infile >> line;
-      std::cout.setf(std::ios::fixed);
-      std::cout.precision(2);
-      std::cout << line << '\n';
-      sum += line;
+#include <iomanip>
+
+int main()
+{
+    //INITIALIZE VARIABLES
+    std::string f;
+    
+    //PROMPT USER TO INPUT FILE NAME
+    std::cout << "Please provide the file name for your donation file: ";
+    std::cin >> f;
+    std::ifstream my_file;
+    
+    //OPEN FILE
+    my_file.open(f);
+    
+    //IF FILE IS OPEN EXECUTE THE FOLLOWING
+    if (my_file.is_open())
+    {
+        //INITIALIZE VARIABLES
+        double line;
+        double total_Don = 0;
+        int don_Price;
+        
+        my_file >> don_Price;
+        my_file.seekg(1, my_file.beg); //START AT 1 TO SKIP # OF DONATORS
+        
+        //LOOP TO DISPLAY DONATION & SUM
+        for (int i = 1; i <= don_Price; i++)
+        {
+            //DISPLAY DONATIONs
+            std::cout << "Donation " << i << ": $";
+            my_file >> line;
+            //DISPLAY PRICE IN 2 DECIMAL POINTS
+            std::cout.precision(2);
+            std::cout.setf(std::ios::fixed);
+            std::cout << line << '\n';
+            
+            //CALCULATE TOTAL DONATION
+            total_Don = total_Don + line;
+        }
+        //CLOSE FILE
+        my_file.close();
+        
+        //DISPLAY TOTAL DONATION
+        std::cout << "\n" << "Total donation: $" << total_Don << "\n";
     }
-    infile.close();
-    std::cout << "Total donation: $" << sum << '\n';
-  } else {
-    std::cout << "Invalid donation file!\n";
-  }
-  return 0;
+    //IF FILE IS INVALID
+    else
+    {
+        std::cout << "Invalid donation file!" << "\n";
+    }
+    return 0;   //END PROGRAM
 }
